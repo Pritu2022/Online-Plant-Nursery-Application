7ab9309a9266b738918915b54438101f711b09c4
Pritu Yadav <101568121+Pritu2022@users.noreply.github.com>
2023-02-28 17:28:22 +0530

    Update OrderService.java

--- The_Green_Country/src/main/java/com/masai/services/OrderService.java
+++ The_Green_Country/src/main/java/com/masai/services/OrderService.java
@@ -15,7 +15,21 @@ import com.masai.models.Orders;
 
 public interface OrderService {
 
+	public Orders buyPlantByPlantId(String sessionId,Integer planterId,Integer quantity) throws CustomerException,PlantNotFoundException,InsufficientQuantity;
 	
+	public Orders buySeedBySeedId(String sessionId,Integer seedId,Integer quantity) throws CustomerException,SeedNotFoundException,InsufficientQuantity;
+	
+	public Orders buyPlanterByPlanterId(String sessionId,Integer PlanterId,Integer quantity) throws CustomerException,PlanterNotFoundException,InsufficientQuantity;
+	
+	public List<Orders> buyPlanterWithSeedByPlanterAndSeedId(String sessionId,Integer planterId,Integer seedId,Integer PlanterQuantity,Integer SeedQuantity) throws CustomerException,PlanterNotFoundException,SeedNotFoundException,InsufficientQuantity;
+	
+	public CartDto visitYourCart(String customerKey) throws CustomerException,OrderException;
+	
+	public String payAmount(String customerKey,Double amount) throws CustomerException,OrderException;
+	
+	public Orders deleteProductByOrderId(String customerKey,Integer orderId) throws CustomerException,OrderException;
+	
+	public Orders updateOrderByOrderId(String customer,Integer orderId,Integer newQuantity) throws CustomerException,OrderException,InsufficientQuantity;
 	
 	
 }
